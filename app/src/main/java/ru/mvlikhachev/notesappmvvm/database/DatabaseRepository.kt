305cff086499378e41d2c7bfa979c3317762acc2
MV.Likhachev <m.lihachev@paysend.com>
2022-03-24 09:40:31 +0400

    Lesson-4

---
+++ app/src/main/java/ru/mvlikhachev/notesappmvvm/database/DatabaseRepository.kt
@@ -0,0 +1,16 @@
+package ru.mvlikhachev.notesappmvvm.database
+
+import androidx.lifecycle.LiveData
+import ru.mvlikhachev.notesappmvvm.model.Note
+
+interface DatabaseRepository {
+
+    val readAll: LiveData<List<Note>>
+
+    suspend fun create(note: Note, onSuccess: ()-> Unit)
+
+    suspend fun update(note: Note, onSuccess: ()-> Unit)
+
+    suspend fun delete(note: Note, onSuccess: ()-> Unit)
+
+}
