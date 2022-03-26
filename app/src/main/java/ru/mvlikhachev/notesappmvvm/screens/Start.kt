f66afc74c8a26cd00adfce8e759230fea41b2a53
MV.Likhachev <m.lihachev@paysend.com>
2022-03-26 17:53:44 +0400

    Refactoring and Lesson-5

--- app/src/main/java/ru/mvlikhachev/notesappmvvm/screens/Start.kt
+++ app/src/main/java/ru/mvlikhachev/notesappmvvm/screens/Start.kt
@@ -22,7 +22,7 @@ import ru.mvlikhachev.notesappmvvm.utils.TYPE_FIREBASE
 import ru.mvlikhachev.notesappmvvm.utils.TYPE_ROOM
 
 @Composable
-fun StartScreen(navController: NavHostController) {
+fun StartScreen(navController: NavHostController, viewModel: MainViewModel) {
     val context = LocalContext.current
     val mViewModel: MainViewModel =
         viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
@@ -69,6 +69,9 @@ fun StartScreen(navController: NavHostController) {
 @Composable
 fun prevStartScreen() {
     NotesAppMVVMTheme {
-        StartScreen(navController = rememberNavController())
+        val context = LocalContext.current
+        val mViewModel: MainViewModel =
+            viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+        StartScreen(navController = rememberNavController(), viewModel = mViewModel)
     }
 }
